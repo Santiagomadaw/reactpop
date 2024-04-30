b6834660ff86d60626b7e3769c9848f44094b100
Santiagomadaw <santiagoma.daw@gmail.com>
2024-04-30 10:15:38 +0200

    first commit-dependencies

---
+++ api/client.ts
@@ -0,0 +1,16 @@
+import axios, { AxiosResponse } from 'axios'; 
+
+
+export const client = axios.create({
+    baseURL: import.meta.env.VITE_API_BASE_URL,
+});
+
+export const setAuthorizationHeader = (token:string) =>
+(client.defaults.headers.common['Authorization'] = `Bearer ${token}`);
+
+export const removeAuthorizationHeader = () => {
+    delete client.defaults.headers.common['Authorization'];
+    };
+
+client.interceptors.response.use((response: AxiosResponse): AxiosResponse =>  response)
+
